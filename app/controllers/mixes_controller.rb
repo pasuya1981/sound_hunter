426aa41a507f275abf2008d4baaff8ef230d8210
pasuya1981 <pasuya1234@hotmail.com>
2014-08-13 00:43:47 +0800

    show page updated

--- app/controllers/mixes_controller.rb
+++ app/controllers/mixes_controller.rb
@@ -8,11 +8,10 @@ class MixesController < ApplicationController
     sort_type = keyword_params[:sort]
     smart_type_in_sym = parse_smart_type smart_type
     sort_type_in_sym = parse_sort_type sort_type
-    keyword = keyword_params[:words]
+    keyword = keyword_params[:words]  || params[:keyword][:words]
     unless keyword.present?
       redirect_to home_path; return; 
     end
-
     remember_search_action smart_type, sort_type
     word_in_ary = keyword.strip.split
     mix_set_search_result = search_mix_set(smart_type_in_sym, word_in_ary, sort_type_in_sym)
@@ -29,12 +28,15 @@ class MixesController < ApplicationController
     @mix_duration = info_hash[:duration]
     @mix_tracks_count = info_hash[:tracks_count]
     @mix_nsfw = info_hash[:nsfw]
-    @mix_artists = []; info_hash[:artists].each { |k, artist| @mix_artists << artist };
+    @mix_artists = [];
+    info_hash[:artists].split.each { |artist| @mix_artists << artist } if info_hash[:artists].kind_of?(String)
+    info_hash[:artists].each { |k, artist| @mix_artists << artist } if info_hash[:artists].kind_of?(Hash)
     @mix_liked_by_current_user = info_hash[:liked_by_current_user]
     @mix_name = info_hash[:name]
     @mix_cover_url = info_hash[:cover_urls][:sq500]
     @plays_count = info_hash[:plays_count]
-    @tag_list_cache = info_hash[:tag_list_cache].split
+    @tag_list_cache = info_hash[:tag_list_cache].gsub(/\//,' ').split(',')
+    @mix_genres = []; info_hash[:genres].each { |k, genre| @mix_genres << genre }
     @first_published_at = info_hash[:first_published_at]
 
     user_hash = info_hash[:user]
@@ -42,9 +44,9 @@ class MixesController < ApplicationController
     @user_login = user_hash[:login]
     @user_path = user_hash[:path]
     @user_web_path = user_hash[:web_path]
-    @user_avatar = user_hash[:avatar_urls][:max250]
+    @user_avatar = user_hash[:avatar_urls][:sq100]
     @user_followed_by_current_user = user_hash[:followed_by_current_user]
-    @location = user_hash[:location]
+    @user_location = user_hash[:location]
     @member_since = user_hash[:member_since]
   end
 
@@ -122,6 +124,7 @@ class MixesController < ApplicationController
     smart_type_in_sym = :artist  if smart_type == '歌手'
     smart_type_in_sym = :keyword if smart_type == '關鍵字'
     smart_type_in_sym = :tags    if smart_type == '標籤'
+    return smart_type.to_sym if [:artist, :keyword, :tags].include?(smart_type.to_sym)
     smart_type_in_sym = :all unless smart_type_in_sym.present?
     smart_type_in_sym
   end
