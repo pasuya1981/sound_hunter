de11ddca4ffd565f7ca1a7c812a6742df27c68ce
pasuya1981 <pasuya1234@hotmail.com>
2014-08-24 05:33:47 +0800

    Arrow can rotate

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,17 +9,17 @@ layout 'mixes'
     init_view_data
   end
 
-  def welcome_ajax
-    if request.xhr?
-      puts "It is ajax request".red
-      init_view_data
-      respond_to do |format|
-        format.js {}
-      end
-    else 
-      # raise "Not a AJAX request!"
-    end
-  end
+  #def welcome_ajax
+  #  if request.xhr?
+  #    puts "It is ajax request".red
+  #    init_view_data
+  #    respond_to do |format|
+  #      format.js {}
+  #    end
+  #  else 
+  #    # raise "Not a AJAX request!"
+  #  end
+  #end
 
   private
 
@@ -27,11 +27,12 @@ layout 'mixes'
     @trending_mixes = EightTracksParser.get_mix_set_by_smart_type(:all).info[:mixes]
     if user_logged_in?
       tracks_user_id = session[:tracks_user_id]
-      favorite_set = EightTracksParser.get_mix_set_by_smart_type(:liked, user_id: tracks_user_id, sort: :popular)
+      favorite_set = EightTracksParser.get_mix_set_by_smart_type(:liked, user_id: tracks_user_id, sort: :hot)
       @favorite_mixes = favorite_set.info[:mixes]
-      recommend_set = EightTracksParser.get_mix_set_by_smart_type(:recommended, user_id: tracks_user_id, sort: :popular)
+      recommend_set = EightTracksParser.get_mix_set_by_smart_type(:recommended, user_id: tracks_user_id, sort: :hot)
       @recommended_mixes = recommend_set.info[:mixes]
     end
+    @new_mixes = EightTracksParser.get_mix_set_by_smart_type(:all, sort: :recent).info[:mixes]
     @tags_ary = EightTracksParser.get_trend_tags
   end
 end
